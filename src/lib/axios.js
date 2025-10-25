4155d46788816210d1c56ad22574fcd38b31cc9c
anthony anup <anup>
2025-10-25 22:22:57 +0530

    updated backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 //change this in development
-const BASE_URL=import.meta.env.MODE=="development"?"http://localhost:3000/api":`${import.meta.env.VITE_BACKEND_URL}/api`
+const BASE_URL=import.meta.env.MODE=="development"?"http://localhost:3000/api":"/api"
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,//if its in the development the url will be http://localhost:3000/api or else it will be "backend_url/api"
--- src/store/authStore.js
+++ src/store/authStore.js
@@ -98,7 +98,7 @@ const useAuthStore = create((set, get) => ({
     },
     connectSocket: async () => {
         const { user, socket } = get()
-        const url=import.meta.env.MODE=="development"?"http://localhost:3000":import.meta.env.VITE_BACKEND_URL
+        const url=import.meta.env.MODE=="development"?"http://localhost:3000":"/"
 
         if (!user || socket?.connected) return //return if there is no auth user or if the user is alreday connected to socket.io
         const newSocket = io(url, {
